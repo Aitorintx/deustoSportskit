e3be69c70f0205b2bd7c7e3f1e5d305bb5485d58
Aitorintx <93260675+Aitorintx@users.noreply.github.com>
2022-06-14 12:47:26 +0200

    a

--- Cliente/main.cpp
+++ Cliente/main.cpp
@@ -8,11 +8,8 @@ int main()
 	Cliente* c=new Cliente("Aitor",1,633091217,"aintxaustim@opendeusto.es","hola","12345");
     ClienteVip* cv=new ClienteVip("Sara",2,722,"sara.hernandez@opendeusto.es","hola","234","standar");
 
-    cout<<"Hola"<<endl;
     c->calculoPrecioFinal(200);
-    cout<<"Luego"<<endl;
     cv->calculoPrecioFinal(500);
-    cout<<"Adios"<<endl;
     
     delete cv;
     delete c;
