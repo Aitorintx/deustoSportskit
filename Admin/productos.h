dd9ee2cc72de7aa3e765bf93769bb3bffab3a1a3
Aitorintx <93260675+Aitorintx@users.noreply.github.com>
2022-06-14 11:54:20 +0200

    estructuras listas

--- Admin/productos.h
+++ Admin/productos.h
@@ -3,8 +3,48 @@
 
 typedef struct 
 {
-    
+    int id,;
+    char* nombre;
+    float precio;
+    int stock;
+    int talla;
 }Calzado;
+
+typedef struct 
+{
+    int id,;
+    char* nombre;
+    float precio;
+    int stock;
+    int talla;
+}Prenda;
+
+typedef struct {
+    int identificativo;
+    int idProducto;
+    int idComprador;
+    int cantidad;
+    float precio;
+} Compra;
+
+typedef struct {
+    char* nombre;
+    int identificativo;
+    int telefono;
+    char* correo;
+    char* direccion;
+    char* contrasena;
+} Comprador;
+
+typedef struct {
+    char* nombre;
+    int identificativo;
+    int telefono;
+    char* correo;
+    char* direccion;
+    char* contrasena;
+    char* nivel;
+} CompradorVip;
 
 
 
